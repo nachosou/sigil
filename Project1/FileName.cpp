755ad2faa2f9079fc6d65853562aa87a02856919
nachosou <nachosou123@gmail.com>
2023-09-19 11:56:07 -0300

    Collitions and ball
    Agregue las colisiones con los marcos y la pelota

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,12 +1,13 @@
 #include "sl.h"
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 struct Paddle
 {
-	float x;
-	float y;
+	float positionX;
+	float positionY;
 	int width;
 	int height;
 	int speed;
@@ -14,21 +15,23 @@ struct Paddle
 
 struct Ball
 {
-	float x;
-	float y;
+	float positionX;
+	float positionY;
 	float speedX;
 	float speedY;
 };
 
 struct Block
 {
-	float x;
-	float y;
+	float positionX;
+	float positionY;
 	int width;
 	int height;
 };
 
 void paddleMovement(Paddle& paddle);
+void ballMovement(Ball& ball, int width, int height);
+void firstBallMovement(Ball& ball);
 
 void main()
 {
@@ -36,12 +39,20 @@ void main()
 	const int height = 768;
 
 	Paddle paddle;
+	Ball ball;
 
-	paddle.x = width * 0.5;
-	paddle.y = 50;
+	paddle.positionX = width * 0.5;
+	paddle.positionY = 50;
 	paddle.speed = 600;
 	paddle.width = 300;
 	paddle.height = 50;
+
+	ball.positionX = width / 2;
+	ball.positionY = height / 2;
+	ball.speedX = 100;
+	ball.speedY = 100;
+
+	bool newScene = true;
 
 	slWindow(width, height, "Simple SIGIL Example", false);
 
@@ -50,9 +61,11 @@ void main()
 		slSetBackColor(0.5, 0.75, 1.0);
 
 		paddleMovement(paddle);
+		ballMovement(ball, width, height);
 
 		slSetForeColor(1, 0, 0, 1);
-		slRectangleFill(paddle.x, paddle.y, paddle.width, paddle.height);
+		slRectangleFill(paddle.positionX, paddle.positionY, paddle.width, paddle.height);
+		slCircleFill(ball.positionX, ball.positionY, 15, 75);
 
 		slRender();
 	}
@@ -64,15 +77,59 @@ void paddleMovement(Paddle& paddle)
 {
 	if (slGetKey(SL_KEY_LEFT))
 	{
-		paddle.x -= paddle.speed * slGetDeltaTime();
+		paddle.positionX -= paddle.speed * slGetDeltaTime();
 	}
 	if (slGetKey(SL_KEY_RIGHT))
 	{
-		paddle.x += paddle.speed * slGetDeltaTime();
+		paddle.positionX += paddle.speed * slGetDeltaTime();
 	}
 }
 
-void ballMovement()
+void ballMovement(Ball& ball, int width, int height)
 {
+	ball.positionX += ball.speedX * slGetDeltaTime();
+	ball.positionY += ball.speedY * slGetDeltaTime();
+}
 
+void firstBallMovement(Ball& ball)
+{
+	srand(time(NULL));
+	int randomDirection = rand() % 2;
+
+	if (randomDirection == 0)
+	{
+		ball.speedX *= -1;
+
+		if (randomDirection == 0)
+		{
+			ball.speedY *= -1;
+		}
+	}
+	else
+	{
+		if (randomDirection == 0)
+		{
+			ball.speedY *= -1;
+		}
+	}
+}
+
+bool collisionWithUpFrame(Ball& ball)
+{
+	if (ball.positionY <= 0)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool collisionWithDownFrame(Ball& ball, int heightScreen)
+{
+	if (ball.positionY >= heightScreen)
+	{
+		return true;
+	}
+
+	return false;
 }
